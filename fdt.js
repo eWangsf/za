3dd97501377a2e9fd3ec71eb44774cfe89c4583a
eWangsf <1342832773@qq.com>
2015-10-11 11:30:02 +0800

    position random and force compute

--- fdt.js
+++ fdt.js
@@ -3,14 +3,15 @@ var canvas,
     positions = [],
     flag = false, //是否存在节点移动：false-无
     ATTACH_FACTOR = 10,
-    CULUN_FACTOR = 1 / (4 * Math.PI);
+    CULUN_FACTOR = 100000 / (4 * Math.PI);
     
 window.onload = function () {
     canvas = document.getElementById('mycanvas');
     ctx = canvas.getContext('2d');
     for (var i = 0; i < data.nodes; i++) {
-        positions.push([400 + 10 * i, 300 + 10 * i]);
+        positions.push([400 + 100 * Math.random() * i, 300 + 100 * Math.random() * i, 0]);
     }
+    console.log(positions);
     Draw();
 }
 
@@ -21,14 +22,20 @@ function Draw() {
 }
 
 function draw(node) {
-    console.log('this id node: ' + node);
-    var attachForce = {'weight': 0, 'degree': 0},
-        repForce = {'weight': 0, 'degree': 0},
+    console.log('node: ' + node + '  position: (' + positions[node][0] + ', ' + positions[node][1] + ', ' + positions[node][2] + ')');
+    var attachForce = {
+            'weight': 0, 
+            'degree': {'x': 0, 'y': 0, 'z': 0}
+        },
+        repForce = {
+            'weight': 0, 
+            'degree': {'x': 0, 'y': 0, 'z': 0}
+        },
         aimPosi = [];
 
     if(node === 0) {
         ctx.beginPath();
-        ctx.arc(positions[0][0], positions[0][1], 4, 0, Math.PI * 2, true);
+        ctx.arc(positions[node][0], positions[node][1], 4, 0, Math.PI * 2, true);
         ctx.closePath();
         ctx.lineWidth = 1;
         ctx.fillStyle = 'green';
@@ -55,20 +62,22 @@ function draw(node) {
         repForce = computeForce(repForce, CalcRepulsionForce(node, i));
     }
     //计算作用在这个节点上的合力的方向
+    var force_total = computeForce(attachForce, repForce);
+    console.log(force_total);
 
     //置flag
     // if ((aimPosi[0] != positions[node][0]) || (aimPosi[1] != positions[node][1])) {
     //     flag = true;
     // }
+    
 }
 
 
 
 function CalcRepulsionForce(x, y) {
-    // console.log(x + " - " + y);
     var weight = 0;
     var degree = {};
-    weight = CULUN_FACTOR * data.links[x]['value'] * data.links[y]['value'] / Math.pow(distance(x, y), 2);
+    weight = (CULUN_FACTOR * data.links[x]['value'] * data.links[y]['value']) / Math.pow(distance(x, y), 2);
     degree = getDegree(x, y);
 
     return {
@@ -80,7 +89,7 @@ function CalcRepulsionForce(x, y) {
 function CalcAttractionForce(x, y) {
     var weight = 0;
     var degree = {};
-    weight = - ATTACH_FACTOR * distance(x, y);
+    weight = ATTACH_FACTOR * distance(x, y);
     degree = getDegree(x, y);
 
     return {
@@ -90,37 +99,44 @@ function CalcAttractionForce(x, y) {
 }
 
 function computeForce(A, B) { //计算两个力的合力
-    var force = {'weight': 0, 'degree': 0};
-    console.log('计算  ' + A + '  和  ' + B + '  的合力');
+    var force = {'weight': 0, 'degree': {'x': 0, 'y': 0, 'z': 0}};
+    var x_force = A['weight'] * A['degree']['x'] + B['weight'] * B['degree']['x'];
+    var y_force = A['weight'] * A['degree']['y'] + B['weight'] * B['degree']['y'];
+    var z_force = A['weight'] * A['degree']['z'] + B['weight'] * B['degree']['z'];
+    force['weight'] = Math.sqrt(Math.pow(x_force, 2) + Math.pow(y_force, 2) + Math.pow(z_force, 2));
+    force['degree']['x'] = x_force /force['weight'];
+    force['degree']['y'] = y_force /force['weight'];
+    force['degree']['z'] = z_force /force['weight'];
     return force;
 }
 
 
 function distance(x, y) {
-    var distance = Math.sqrt(Math.pow(positions[x][0] - positions[y][0]) + Math.pow(positions[x][1] - positions[y][1]) + Math.pow(positions[x][2] - positions[y][2]));
+    var distance = Math.sqrt(Math.pow(positions[x][0] - positions[y][0], 2) + Math.pow(positions[x][1] - positions[y][1], 2) + Math.pow(positions[x][2] - positions[y][2], 2));
     return distance;
 }
 
 function getDegree(x, y) {
     var degree = {'x': 0, 'y': 0, 'z': 0};
-    var vec = {'x': positions[x][0] - positions[y][0], 'y': positions[x][1] - positions[y][1], 'z': positions[x][2] - positions[y][2]};
+    var vec = {'x': positions[y][0] - positions[x][0], 'y': positions[y][1] - positions[x][1], 'z': positions[y][2] - positions[x][2]};
     var x = {'x': 1, 'y': 0, 'z': 0};
     var y = {'x': 0, 'y': 1, 'z': 0};
     var z = {'x': 0, 'y': 0, 'z': 1};
-    degree['x'] = vec['x'] * x['x'] / (Math.sqrt(Math.pow(vec['x']) + Math.pow(vec['y']) + Math.pow(vec['z'])));
-    degree['y'] = vec['y'] * y['y'] / (Math.sqrt(Math.pow(vec['x']) + Math.pow(vec['y']) + Math.pow(vec['z'])));
-    degree['z'] = vec['z'] * z['z'] / (Math.sqrt(Math.pow(vec['x']) + Math.pow(vec['y']) + Math.pow(vec['z'])));
-    // console.log('~~~~~~~' + degree['x']);
+    degree['x'] = (vec['x'] * x['x']) / (Math.sqrt(Math.pow(vec['x'], 2) + Math.pow(vec['y'], 2) + Math.pow(vec['z'], 2)));
+    degree['y'] = (vec['y'] * y['y']) / (Math.sqrt(Math.pow(vec['x'], 2) + Math.pow(vec['y'], 2) + Math.pow(vec['z'], 2)));
+    degree['z'] = (vec['z'] * z['z']) / (Math.sqrt(Math.pow(vec['x'], 2) + Math.pow(vec['y'], 2) + Math.pow(vec['z'], 2)));
     return degree;
 }
 
 
 var data = {
-  "nodes": 3,
+  "nodes": 5,
   "links": [
     {"source": 0, "target": 1, "value": 1},
     {"source": 1, "target": 2, "value": 1},
-    {"source": 0, "target": 2, "value": 1}
+    {"source": 0, "target": 2, "value": 1},
+    {"source": 1, "target": 3, "value": 1},
+    {"source": 2, "target": 4, "value": 1}
   ]
 }
 
