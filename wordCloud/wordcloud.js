42b06101e43fed66282773ed6c59637056570256
eWangsf <1342832773@qq.com>
2015-10-15 23:06:44 +0800

    with degree random

--- wordCloud/wordcloud.js
+++ wordCloud/wordcloud.js
@@ -1,47 +1,69 @@
 var canvas,
     ctx,
     list;
+var paras = [];
 
 window.onload = function () {
-    // console.log(document.getElementById('canvas').getAttribute('width'));
     canvas = document.getElementById("canvas");
-    canvas.style.backgroundColor = option.backgroundColor;
+    var canvasWidth = canvas.getAttribute('width');
+    var canvasHeight = canvas.getAttribute('height');
     ctx = canvas.getContext("2d"); 
     list = option.list;
+    canvas.style.backgroundColor = option.backgroundColor;
     for(var i = 0; i < list.length; i++) {
         putWord(list[i][0], list[i][1]);
     }
+    console.log(option.gridSize);
 }
 
 
 
 function putWord(key, weight) {
+    //获取绘制参数
+    var position = [parseInt(Math.random() * 600) + 200, parseInt(Math.random() * 400) + 200];
+    var degree = (Math.random() - 0.5) * Math.PI;
 
-    ctx.beginPath();
- 
-ctx.fillStyle = option.color();
-ctx.font = option.weightFactor(weight) + "px " + option.fontFamily;
-ctx.textAlign = option.align;
-
-var position = [Math.random() * 1000, Math.random() * 400];
-ctx.fillText(key, position[0], position[1]);
- 
-ctx.save();
-ctx.translate(position[0], position[1]);
-ctx.rotate(Math.PI / 6);
-ctx.fillText(key, 0, 0);
-ctx.restore();
-    // ctx.font = option.weightFactor(weight) + "px " + option.fontFamily;
-    // ctx.fillStyle = option.color();
-    // ctx.textBaseline = option.align;
-    // var position = [Math.random() * 1000, Math.random() * 400];
-    // ctx.fillText(key, position[0], position[1]);
-    // var degree = (Math.random() - 0.5) * 2 * Math.PI;
-    // ctx.rotate(degree);
-    // ctx.save();
     
-    // ctx.restore();
-    // ctx.strokeText(list[1][0], 500, 300);
+    // for(var i = 0; i < paras.length; i++) {
+    //     ctx.beginPath();
+    //     ctx.fillStyle = option.color();
+    //     ctx.font = option.weightFactor(weight) + "px " + option.fontFamily;
+    //     ctx.textAlign = option.align;
+    //     ctx.save();
+    //     ctx.translate(paras[i][0], paras[i][1]);
+    //     ctx.rotate(paras[i][2]);
+    //     ctx.fillText(key, 0, 0);
+    //     ctx.restore();
+    //     if(ctx.isPointInPath(300, 400)) {
+    //         alert('hh');
+    //     }
+    //     // ctx.closePath();
+
+    // }
+   
+        ctx.fillStyle = option.color();
+        ctx.font = option.weightFactor(weight) + "px " + option.fontFamily;
+        ctx.textAlign = option.align;
+        ctx.save();
+        ctx.translate(position[0], position[1]);
+        ctx.rotate(degree);
+        ctx.fillText(key, 0, 0);
+        ctx.restore();
+        // paras.push([position[0], position[1], degree]);
+    
+    
+    
+    // var thisTextWidth = ctx.measureText(key).width;
+    
+    // var points = [
+    //     [],
+    //     [],
+    //     [],
+    //     []
+    // ];
+
+    
+
 }
 
 
@@ -55,15 +77,20 @@ var option = {
 
     list: [
         ["Hello", 180],
+        ["Java", 120],
         ["intersection", 80],
-        ["particular", 80],
-        ["word", 70],
-        ["shape", 65],
+        ["expensive", 70],
+        ["particular", 65],
+        ["word", 60],
+        ["shape", 60],
         ["bottleneck", 55],
         ["index", 50],
-        ["box", 50],
+        ["box", 40],
         ["position", 30],
-        ["structure", 25],
+        ["structure", 30],
+        ["create", 30],
+        ["recently", 25],
+        ["C++", 25],
     ],
 
     weightFactor: function (size) {
@@ -71,12 +98,13 @@ var option = {
       return size;
     },
     color: function () {
-        var color = 'rgb(' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ')';
-        // console.log(color);
+        var color = 'rgb(' + (parseInt(Math.random() * 205) + 50) + ', ' + (parseInt(Math.random() * 205) + 50) + ', ' + (parseInt(Math.random() * 205) + 50) + ')';
         return color;
     },
     gridSize: Math.round(16 * parseInt(document.getElementById('canvas').getAttribute("width")) / 1024)
 }
+
+
 
 
 
