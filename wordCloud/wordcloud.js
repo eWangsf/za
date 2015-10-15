58ab965b26945a5c58b1fb418ed36a5bcbfd96e8
eWangsf <1342832773@qq.com>
2015-10-15 13:37:16 +0800

    with text rotate

--- wordCloud/wordcloud.js
+++ wordCloud/wordcloud.js
@@ -3,50 +3,79 @@ var canvas,
     list;
 
 window.onload = function () {
+    // console.log(document.getElementById('canvas').getAttribute('width'));
     canvas = document.getElementById("canvas");
+    canvas.style.backgroundColor = option.backgroundColor;
     ctx = canvas.getContext("2d"); 
     list = option.list;
-    
-    ctx.font = "60px Courier New";
-    //设置字体填充颜色
-    ctx.fillStyle = option.color();
-    //从坐标点(50,50)开始绘制文字
-    ctx.fillText(list[0][0], 200, 200);
-
+    for(var i = 0; i < list.length; i++) {
+        putWord(list[i][0], list[i][1]);
+    }
 }
 
 
 
-function putWord(canvas) {
+function putWord(key, weight) {
 
+    ctx.beginPath();
+ 
+ctx.fillStyle = option.color();
+ctx.font = option.weightFactor(weight) + "px " + option.fontFamily;
+ctx.textAlign = option.align;
+
+var position = [Math.random() * 1000, Math.random() * 400];
+ctx.fillText(key, position[0], position[1]);
+ 
+ctx.save();
+ctx.translate(position[0], position[1]);
+ctx.rotate(Math.PI / 6);
+ctx.fillText(key, 0, 0);
+ctx.restore();
+    // ctx.font = option.weightFactor(weight) + "px " + option.fontFamily;
+    // ctx.fillStyle = option.color();
+    // ctx.textBaseline = option.align;
+    // var position = [Math.random() * 1000, Math.random() * 400];
+    // ctx.fillText(key, position[0], position[1]);
+    // var degree = (Math.random() - 0.5) * 2 * Math.PI;
+    // ctx.rotate(degree);
+    // ctx.save();
+    
+    // ctx.restore();
+    // ctx.strokeText(list[1][0], 500, 300);
 }
 
 
 var option = {
-    list: [
-        ["Hello", 8],
-        ["intersection", 6],
-        ["particular", 6],
-        ["word", 3],
-        ["shape", 2],
-        ["bottleneck", 2],
-        ["index", 1],
-        ["box", 1],
-        ["position", 1],
-        ["structure", 1],
-    ],
-    gridSize: Math.round(16 * parseInt(document.getElementById('canvas').getAttribute("width")) / 1024),
-    weightFactor: function (size) {
-      return Math.pow(size, 2.3) * $('#canvas').style.width / 1024;
-    },
+    backgroundColor: '#000',
+    align: 'top',
+    fontFamily: '微软雅黑',
     minRotation: 1.58,
     maxRotation: 1.58,
     rotateRatio: 0.3,
-    color: function () {
-      return ['blue','rgb(95, 95, 253)','rgb(146, 146, 255)','rgb(169, 169, 226)','rgb(168, 217, 252)'][Math.floor(Math.random()*5)]
+
+    list: [
+        ["Hello", 180],
+        ["intersection", 80],
+        ["particular", 80],
+        ["word", 70],
+        ["shape", 65],
+        ["bottleneck", 55],
+        ["index", 50],
+        ["box", 50],
+        ["position", 30],
+        ["structure", 25],
+    ],
+
+    weightFactor: function (size) {
+      // return Math.pow(size, 2.3) * $('#canvas').style.width / 1024;
+      return size;
     },
-    fontFamily: '微软雅黑',
-    backgroundColor: '#fff'
+    color: function () {
+        var color = 'rgb(' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ')';
+        // console.log(color);
+        return color;
+    },
+    gridSize: Math.round(16 * parseInt(document.getElementById('canvas').getAttribute("width")) / 1024)
 }
 
 
