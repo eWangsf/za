fe0d2b6200c1e78df8b373aa49ea3d2d18458077
eWangsf <1342832773@qq.com>
2015-10-21 21:34:54 +0800

    d3 start

--- d3/d3_begin.js
+++ d3/d3_begin.js
@@ -1,1 +1,12 @@
-d3.select("body").style("background-color", "black");
+// d3.select("body").style("background-color", "black");
+
+// d3.select("body").append("p").text("New paragraph!");
+// d3.csv('csv.csv', function (data) {
+//     console.log(data);
+// });
+
+d3.select('body').selectAll('div').data([1, 2, 3, 4, 5]).enter().append('div').text(function (d) {
+    return d;
+});
+
+
