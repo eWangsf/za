31ea244db4c0ad316e54c2104419700178026d0d
eWangsf <1342832773@qq.com>
2015-10-23 10:26:00 +0800

    .

--- wordCloud/wordcloud.js
+++ wordCloud/wordcloud.js
@@ -1,45 +1,44 @@
 var svg,
-    list;
-
-var paras = [];
+    list,
+    paras = [];
 
 window.onload = function () {
     svg = document.getElementById("svg");
     list = option.list;
-    // putWord(0);
-    for(var i = 0; i < list.length; i++) {
+    for (var i = 0; i < list.length; i++) {
         putWord(i);
     }
 }
 
-
-
 function putWord(i) {
-    var position = [parseInt(Math.random() * 1000) + 50, parseInt(Math.random() * 400) + 200];
+    var position = [parseInt(Math.random() * 1000) + 50, parseInt(Math.random() * 500) + 200];
     var degree = ((Math.random() - 0.5) * 180).toFixed(2);
 
     var textStr = "<text x='" + position[0] + "' y='" + position[1] 
                 + "' style='fill: " + option.color() 
                 + "; font-size: " + option.weightFactor(list[i][1]) + "px;text-anchor: start;dominant-baseline: middle;' " 
-                + "transform= 'rotate(" + degree + ", "+ position[0] + ", " + position[1] + ")'" 
+                + "transform= 'rotate(" + degree + " " + position[0] + ", " + position[1] + ")'" 
                 + ">" + list[i][0] + "</text>";    
     svg.innerHTML = svg.innerHTML + textStr;
+    console.log(list[i][0] + "  " + position[0] + "  " + position[1]);
 
     paras.push([position, degree]);
+
     var result;
     do{
         var x = parseInt(Math.random() * 1000) + 50;
-        var y = parseInt(Math.random() * 400) + 200;
-        paras[paras.length - 1][0][0] = x;
-        paras[paras.length - 1][0][1] = y;
+        var y = parseInt(Math.random() * 500) + 200;
+        var last = paras.length - 1;
+        paras[last][0][0] = x;
+        paras[last][0][1] = y;
 
         var texts = document.getElementsByTagName('text');
-        var last = texts.length - 1;
+        
         texts[last].setAttribute('x', x);
         texts[last].setAttribute('y', y);
         texts[last].setAttribute('transform', 'rotate(' + degree + ' ' + x + ', ' + y + ')');
-        // console.log(list[paras.length - 1][0] + " changes ");
-        result = fitPosition();
+        console.log(list[paras.length - 1][0] + " changes ");
+        result = fitPosition(); //result： true-无覆盖单词  false-检测到覆盖
     } while(!result);
 
 }
@@ -54,29 +53,26 @@ function fitPosition() {
     var positionYLast = paras[lastIndex][0][1];
     var textDegreeLast = ((Math.PI * paras[lastIndex][1]) / 180);
 
-    var textcenterLast = [positionXLast + 0.5 * (lastWidth*Math.cos(textDegreeLast) - lastHeight*Math.sin(textDegreeLast)), 
-                        positionYLast + 0.5 * (lastWidth*Math.sin(textDegreeLast) + lastHeight*Math.cos(textDegreeLast))];
+    var textcenterLast = [positionXLast + 0.5 * lastWidth * Math.cos(textDegreeLast) - 0.5 * lastHeight * Math.sin(textDegreeLast), 
+                        positionYLast + 0.5 * lastWidth * Math.sin(textDegreeLast) + 0.5 * lastHeight * Math.cos(textDegreeLast)];
 
-    for(var i = 0; i < paras.length - 1; i++) {
+    for(var i = 0; i < lastIndex; i++) {
         var textWidth = texts[i].offsetWidth;
         var textHeight = texts[i].offsetHeight;
         var positionX = paras[i][0][0];
         var positionY = paras[i][0][1];
         var textDegree = ((Math.PI * paras[i][1]) / 180);
 
-        var textcenter = [positionX + 0.5 * (textWidth*Math.cos(textDegree) - textHeight*Math.sin(textDegree)), 
-                        positionY + 0.5 * (textWidth*Math.sin(textDegree) + textHeight*Math.cos(textDegree))];
+        var textcenter = [positionX + 0.5 * textWidth * Math.cos(textDegree) - 0.5 * textHeight * Math.sin(textDegree), 
+                        positionY + 0.5 * textWidth * Math.sin(textDegree) + 0.5 * textHeight * Math.cos(textDegree)];
 
-        if((Math.abs(textcenter[0] - textcenterLast[0]) > (0.5 * (textWidth + lastWidth))) || (Math.abs(textcenter[1] - textcenterLast[1]) > (0.5 * (textHeight + lastHeight)))) {
+        if ((Math.abs(textcenter[0] - textcenterLast[0]) > (0.5 * (textWidth + lastWidth))) || (Math.abs(textcenter[1] - textcenterLast[1]) > (0.5 * (textHeight + lastHeight)))) {
+            //第i个word和新加入的word不相交
             continue;
         } else {
             //检测到相交
             return false;
         }
-        // var point1 = [positionX, positionY];
-        // var point2 = [positionX + textWidth * Math.cos(textDegree), positionY + textWidth * Math.sin(textDegree)];
-        // var point3 = [point2[0] - textHeight * Math.sin(textDegree), point2[1] + textHeight * Math.cos(textDegree)];
-        // var point4 = [positionX - textHeight * Math.sin(textDegree), positionY + textHeight * Math.cos(textDegree)];
     }
     return true;
 
