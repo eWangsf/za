efc78b317031365fb52a7bbd743b17bd95101fc2
eWangsf <1342832773@qq.com>
2015-10-29 15:39:38 +0800

    force directed tree v1.0

--- fdt/fdt_raw/fdt_svg.js
+++ fdt/fdt_raw/fdt_svg.js
@@ -15,8 +15,8 @@ var radium = 8,
 
 var maxOffset = 50,
     zero = 0.001,
-    charge = 30,
-    linkDistance = 20,
+    charge = 3,
+    linkDistance = 15,
     linkStrength = .0001,
     gravity = 0.1;
 
@@ -36,39 +36,41 @@ window.onload = function () {
     linkNumber = links.length;
 
     //bind
-    for (var i = 0; i < circleNumber; i++) {
-        circles[i].asSrc = [];
-        circles[i].asTar = [];
-    }
+    // for (var i = 0; i < circleNumber; i++) {
+    //     circles[i].asSrc = [];
+    //     circles[i].asTar = [];
+    // }
 
-    for (var i = 0; i < linkNumber; i++) {
-        var obj = links[i];
-        circles[obj.source].asSrc.push(i);
-        circles[obj.target].asTar.push(i);
-    }
-
-    // console.log(circles[0].asSrc);
-    // console.log(circles[0].asTar);
-
+    // for (var i = 0; i < linkNumber; i++) {
+    //     var obj = links[i];
+    //     circles[obj.source].asSrc.push(i);
+    //     circles[obj.target].asTar.push(i);
+    // }
+    
     //初始化元素位置
+    var circlePos = [];
     var circleStr = '';
     for (var i = 0; i < circleNumber; i++) { 
         var x = Math.random() * (width - 2 * padding) + padding;
         var y = Math.random() * (height - 2 * padding) + padding;
-        circleStr += '<circle cx="' + x + '" cy="' + y + '" r="' + radium + '" fill="' + circlecolor + '" onclick="circlemoveFunc(this)"/>'
+        circlePos.push([x, y]);
     }
-    svg.innerHTML = circleStr;
-
-    circledoms = document.getElementsByTagName('circle');
 
     var linkStr = '';
     for (var i = 0; i < linkNumber; i++) {
         var source = links[i].source;
         var target = links[i].target;
-        linkStr += '<line x1="' + circledoms[source].getAttribute('cx') + '" y1="' + circledoms[source].getAttribute('cy') + '" x2="' + circledoms[target].getAttribute('cx') + '" y2="' + circledoms[target].getAttribute('cy') + '" style="stroke:' + linkcolor + ';stroke-width: ' + linkwidth + '"/>'
+        linkStr += '<line x1="' + circlePos[source][0] + '" y1="' + circlePos[source][1] + '" x2="' + circlePos[target][0] + '" y2="' + circlePos[target][1] + '" style="stroke:' + linkcolor + ';stroke-width: ' + linkwidth + '"/>'
     }
     svg.innerHTML += linkStr;
     linkdoms = document.getElementsByTagName('line');
+
+    var circleStr = '';
+    for (var i = 0; i < circleNumber; i++) {
+        circleStr += '<circle cx="' + circlePos[i][0] + '" cy="' + circlePos[i][1] + '" r="' + radium + '" fill="' + circlecolor + '" />'
+    }
+    svg.innerHTML += circleStr;
+    circledoms = document.getElementsByTagName('circle');
 
     // //layout algorithm
     toBeBalance();
@@ -105,9 +107,7 @@ function toBeBalance() {
         thislink.setAttribute('y2', y2);
     }
 
-    //重力更新
-
-    setTimeout(toBeBalance, 1000);
+    setTimeout(toBeBalance, 15);
 }
 
 function repulse(node1, node2) {
@@ -126,51 +126,29 @@ function repulse(node1, node2) {
 
     circledoms[node1].setAttribute('cx', parseFloat(circledoms[node1].getAttribute('cx')) - x);
     circledoms[node1].setAttribute('cy', parseFloat(circledoms[node1].getAttribute('cy')) - y);
-    // for (var i = 0; i < circles[node1].asSrc.length; i++) {
-    //     var thislink = linkdoms[circles[node1].asSrc[i]];
-    //     thislink.setAttribute('x1', thislink.getAttribute('x1') - x);
-    //     thislink.setAttribute('y1', thislink.getAttribute('y1') - y);
-    // }
     
     circledoms[node2].setAttribute('cx', parseFloat(circledoms[node2].getAttribute('cx')) + x);
     circledoms[node2].setAttribute('cy', parseFloat(circledoms[node2].getAttribute('cy')) + y);
-    // for (var i = 0; i < circles[node2].asTar.length; i++) {
-    //     var thislink = linkdoms[circles[node2].asTar[i]];
-    //     thislink.setAttribute('x2', thislink.getAttribute('x2') + x);
-    //     thislink.setAttribute('y2', thislink.getAttribute('y2') + y);
-    // }
-    // console.log("x: " + repweight * dx + "  y: " + repweight * dy);
 
     return true;
 }
 
 function attract(link) {
-    console.log(links[link]);
+    // console.log(links[link]);
     var srcircle = circledoms[links[link].source],
         tarcircle = circledoms[links[link].target],
         sr_x = linkdoms[link].getAttribute('x1') || 0,
         sr_y = linkdoms[link].getAttribute('y1') || 0,
         tar_x = linkdoms[link].getAttribute('x2') || 0,
         tar_y = linkdoms[link].getAttribute('y2') || 0,
-        dx = (tar_x - sr_x) - Math.sqrt(linkDistance), dy = (tar_y - sr_y) - Math.sqrt(linkDistance), dn = dx * dx + dy * dy,
+        dx = (tar_x - sr_x) - (linkDistance), dy = (tar_y - sr_y) - (linkDistance), dn = dx * dx + dy * dy,
         attractweight = linkStrength * Math.sqrt(dn);
 
     srcircle.setAttribute('cx', parseFloat(srcircle.getAttribute('cx')) + attractweight * dx);
     srcircle.setAttribute('cy', parseFloat(srcircle.getAttribute('cy')) + attractweight * dy);
     tarcircle.setAttribute('cx', parseFloat(tarcircle.getAttribute('cx')) - attractweight * dx);
     tarcircle.setAttribute('cy', parseFloat(tarcircle.getAttribute('cy')) - attractweight * dy);
-    console.log('dx= ' + dx + '   dy= ' + dy + '   attr_x= ' + attractweight * dx + '   attr_y= ' + attractweight * dy);
-    // console.log('sr: x= ' + sr_x + '  y= ' + sr_y);
-    // console.log('tar: x= ' + tar_x + '  y= ' + tar_y);
-    // srcircle.setAttribute('cx', parseFloat(srcircle.getAttribute('cx')) + attractweight * Math.sqrt(dx));
-    // srcircle.setAttribute('cy', parseFloat(srcircle.getAttribute('cy')) + repweight * Math.sqrt(dy));
+    // console.log('dx= ' + dx + '   dy= ' + dy + '   attr_x= ' + attractweight * dx + '   attr_y= ' + attractweight * dy);
         
     // console.log(repweight * Math.sqrt(dx));
-}
-
-
-
-
-function circlemoveFunc() {
-    alert("Hh");
 }
