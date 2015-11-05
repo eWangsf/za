954e79358cb1e0dafd7cf7312abd2b76a072d7ba
eWangsf <1342832773@qq.com>
2015-11-05 16:25:12 +0800

    stackedgraph in raw js - wiggle

--- stackedGraph/raw/stackedgraph.js
+++ stackedGraph/raw/stackedgraph.js
@@ -3,70 +3,108 @@ var svg,
     width,
     height;
 
-var n = 20,
-    m = 200;
+var btn,
+    graph;
 
-var data = [],
-    colors = [
-        "rgb(74, 73, 93)", "rgb(90, 88, 118)", 
-        "rgb(121, 119, 165)", "rgb(99, 98, 132)",
-        "rgb(97, 95, 129)", "rgb(88, 86, 115)"
-    ];
-var yscale,
-    xscale;
 window.onload = function () {
     svg = document.getElementsByTagName('svg')[0];
     width = svg.getAttribute('width');
     height = svg.getAttribute('height');
-    for(var i = 0; i < n; i++) {
-        data.push(bumpLayer(m));
-    }
-
-    stack();
-
-    yscale = new yScale(data);
-    yscale.init();
-    xscale = new xScale();
-    xscale.setdomain([0, m]);
-    xscale.setrange([0, width]);
-
-    draw();
-}
-
-function stack() {
-    for(var j = 0; j < data[0].length; j++) {
-        data[0][j].y0 = 0;
-        data[0][j].y1 = data[0][j].y;
-    }
-    for(var i = 1; i < data.length; i++) {
-        for(var j = 0; j < data[0].length; j++) {
-            data[i][j].y0 = data[i - 1][j].y0 + data[i - 1][j].y;
-            data[i][j].y1 = data[i][j].y0 + data[i][j].y;
-        }
-    }
-}
-
-function draw() {
-    var length = data[0].length;
-    for(var i = 0; i < data.length; i++) {
-
-        var pathStr = '<path d="';
-        for(var j = 0; j < length; j++) {
-            if (j === 0) {
-                pathStr += 'M' + xscale.xscale(data[i][j].x) + ', ' + yscale.yscale(data[i][j].y1);
-                continue;
-            }
-            pathStr += 'L' + xscale.xscale(data[i][j].x) + ', ' + yscale.yscale(data[i][j].y1);
-        }
-        for(var j = length - 1; j >= 0; j--) {
-            pathStr += 'L' + xscale.xscale(data[i][j].x) + ', ' + yscale.yscale(data[i][j].y0);
-        }
-
-        var color = parseInt(Math.random() * 6);
-        pathStr += '" fill="' + colors[color] + '"/>';
-        svg.innerHTML = svg.innerHTML + pathStr;
-    }
+    btn = document.getElementsByTagName('button')[0];
     
+    // btn click
+    btn.onclick = update;
+
+    graph = new stackedGraph('wiggle');
+    graph.start();
+    graph.render(svg);
+
+}
+
+function stackedGraph(type) {
+    this.data = [];
+    this.layerNum = 20;
+    this.layerPoints = 200;
+    this.colors = [
+        "rgb(74, 73, 93)", "rgb(90, 88, 118)", 
+        "rgb(121, 119, 165)", "rgb(99, 98, 132)",
+        "rgb(97, 95, 129)", "rgb(88, 86, 115)"
+    ];
+    this.type = type;
+    this.xscale = new xScale();
+    this.yscale = new yScale([]);
+}
+
+stackedGraph.prototype = {
+    start: function () {
+        this.data = [];
+        for(var i = 0; i < this.layerNum; i++) {
+            this.data.push(bumpLayer(this.layerPoints));
+        }
+        switch(this.type) {
+        case 'wiggle': {
+            for(var j = 0; j < this.layerPoints; j++) {
+                // data[0][j].y0 = 0;
+                var tmp = 0;
+                for (var k = 0; k < this.layerNum; k++) {
+                    tmp += (this.layerNum - k + 1) * this.data[k][j].y;
+                }
+                tmp *= -1 / (this.layerNum + 1);
+                this.data[0][j].y0 = tmp;
+                this.data[0][j].y1 = this.data[0][j].y0 + this.data[0][j].y;
+            }
+            break;
+        }
+        }
+        
+        for(var i = 1; i < this.layerNum; i++) {
+            for(var j = 0; j < this.layerPoints; j++) {
+                this.data[i][j].y0 = this.data[i - 1][j].y0 + this.data[i - 1][j].y;
+                this.data[i][j].y1 = this.data[i][j].y0 + this.data[i][j].y;
+            }
+        }
+
+        // scales
+        this.yscale = new yScale(this.data);
+        this.yscale.init();
+        this.xscale.setdomain([0, this.layerPoints]);
+        this.xscale.setrange([0, width]);
+
+    },
+    getData: function () {
+        return this.data;
+    },
+    render: function (svg) {
+        svg.innerHTML = "";
+        var length = this.layerPoints,
+            data = this.data;
+        for(var i = 0; i < this.layerNum; i++) {
+
+            var pathStr = '<path d="';
+            for(var j = 0; j < length; j++) {
+                if (j === 0) {
+                    pathStr += 'M' + this.xscale.xscale(data[i][j].x) + ', ' + this.yscale.yscale(data[i][j].y1);
+                    continue;
+                }
+                pathStr += 'L' + this.xscale.xscale(data[i][j].x) + ', ' + this.yscale.yscale(data[i][j].y1);
+            }
+            for(var j = length - 1; j >= 0; j--) {
+                pathStr += 'L' + this.xscale.xscale(data[i][j].x) + ', ' + this.yscale.yscale(data[i][j].y0);
+            }
+
+            var color = parseInt(Math.random() * 6);
+            pathStr += '" fill="' + this.colors[color] + '"/>';
+            svg.innerHTML = svg.innerHTML + pathStr;
+        }
+    }
+}
+
+function update() {
+    graph = new stackedGraph('wiggle');
+    graph.start();
+    
+    graph.render(svg);
+
 }
 
 
@@ -90,7 +128,8 @@ xScale.prototype = {
 }
 
 function yScale(dataset) {
-    this.max = 0;
+    this.max = Number.MIN_VALUE;
+    this.min = Number.MAX_VALUE;
     this.data = dataset;
 }
 
@@ -100,14 +139,16 @@ yScale.prototype = {
         for(var i = 0; i < dataset.length; i++) {
             for(var j = 0; j < dataset[i].length; j++) {
                 if(dataset[i][j].y1 > this.max) {
-
                     this.max = dataset[i][j].y1;
+                }
+                if(dataset[i][j].y1 < this.min) {
+                    this.min = dataset[i][j].y1;
                 }
             }
         }
     },
     yscale: function (d) {
-        return height - (d / this.max) * height * 0.8;
+        return height - ((d - this.min) / (this.max - this.min)) * height * 0.9;
     }
 }
 
