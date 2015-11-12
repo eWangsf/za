8855901cf2d6df08d687c3420641f658d27a1458
eWangsf <1342832773@qq.com>
2015-11-12 12:15:57 +0800

    collaTree in raw

---
+++ collaTree/raw/collaTree.js
@@ -0,0 +1,18 @@
+var svg,
+    g,
+    width,
+    height;
+
+window.onload = function () {
+    svg = document.getElementsByTagName('svg')[0];
+    g = svg.getElementsByTagName('g')[0];
+    width = svg.getAttribute('width');
+    height = svg.getAttribute('height');
+    // console.log(g);
+    // console.log(data);
+    // console.log(width);
+    // console.log(height);
+    // document.write('ready');
+
+
+}
