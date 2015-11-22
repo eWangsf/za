cc1334fb2d2805b3dd802fe811a6ea11a057ab73
eWangsf <1342832773@qq.com>
2015-11-22 15:15:59 +0800

    epicyclic gearing in raw done

--- epiGear/raw/epiGear.js
+++ epiGear/raw/epiGear.js
@@ -11,22 +11,29 @@ var settings = {
     'sun': {
         num: 1,
         radius: radius,
-        teeth: 16
+        teeth: 16,
+        rotate_r: -radius
     },
     'planet': {
         num: 3,
         radius: -radius * 2,
-        teeth: 32
+        teeth: 32,
+        rotate_r: Infinity
     },
     'annulus': {
         num: 1,
         radius: -radius * 5,
         teeth: 80,
-        annulus: true
+        annulus: true,
+        rotate_r: radius * 5
     }
 };
 
+var start,
+    offset = 0;
+
 var epigear;
+var timeEvent;
 
 
 window.onload = function () {
@@ -34,26 +41,31 @@ window.onload = function () {
     width = svg.getAttribute('width');
     height = svg.getAttribute('height');
     svg.innerHTML = '<g transform="translate(' + (width / 2) + ', ' + (height / 2) + ')scale(0.55)" ></g>';
-    var g = svg.getElementsByTagName('g')[0];
-    g.innerHTML = '<g></g>';
-    g = g.getElementsByTagName('g')[0];
-    g.innerHTML = '<g transform="rotate(0)"></g>';
-    var g1 = g.getElementsByTagName('g')[0];
+    var g1 = svg.getElementsByTagName('g')[0];
+    g1.innerHTML = '<g transform="rotate(0)"></g>';
+    var g2 = g1.getElementsByTagName('g')[0];
+    g2.innerHTML = '<g transform="rotate(0)"></g>';
+    var g3 = g2.getElementsByTagName('g')[0];
 
+    start = Date.now();
 
-    epigear = new epiGear(settings, g1, speed);
+    epigear = new epiGear(settings, g1, g2, g3, speed);
     epigear.start();
-
 }
 
-function epiGear(settings, container, speed) {
+function epiGear(settings, g1, g2, g3, speed) {
     this.settings = settings;
-    this.container = container;
+    this.annulusg = g1;
+    this.planetg = g2;
+    this.sung = g3;
     this.speed = speed;
+    this.now_r = Infinity;
 }
 
 epiGear.prototype.start = function () {
     this.renderCircles();
+    this.addEvent();
+    timeEvent = setInterval(this.rotate(), 20);
 }
 
 epiGear.prototype.renderCircles = function () {
@@ -62,17 +74,17 @@ epiGear.prototype.renderCircles = function () {
     var pathstr = '';
     for(var key in settings) {
         if(key === 'planet') {
-            pathstr += '<g ' + ('transform="translate(0, ' + (-radius * 3) + ')"') + ' class="' + key + '"><path d="' + gear(settings[key]) + 'Z" transform="rotate(0)"></path></g>';
-            pathstr += '<g ' + ('transform="translate(' + (-radius * 3 * x) + ', ' + (-radius * 3 * y) + ')"') + ' class="' + key + '"><path d="' + gear(settings[key]) + 'Z" transform="rotate(0)"></path></g>';
-            pathstr += '<g ' + ('transform="translate(' + (radius * 3 * x) + ', ' + (-radius * 3 * y) + ')"') + ' class="' + key + '"><path d="' + gear(settings[key]) + 'Z" transform="rotate(0)"></path></g>';
+            pathstr += '<g ' + ('transform="translate(0, ' + (-radius * 3) + ')"') + ' class="' + key + '"><path d="' + this.gear(settings[key]) + 'Z" transform="rotate(0)"></path></g>';
+            pathstr += '<g ' + ('transform="translate(' + (-radius * 3 * x) + ', ' + (-radius * 3 * y) + ')"') + ' class="' + key + '"><path d="' + this.gear(settings[key]) + 'Z" transform="rotate(0)"></path></g>';
+            pathstr += '<g ' + ('transform="translate(' + (radius * 3 * x) + ', ' + (-radius * 3 * y) + ')"') + ' class="' + key + '"><path d="' + this.gear(settings[key]) + 'Z" transform="rotate(0)"></path></g>';
         } else {
-             pathstr += '<g class="' + key + '"><path d="' + gear(settings[key]) + 'Z" transform="rotate(0)"></path></g>';
+             pathstr += '<g class="' + key + '"><path d="' + this.gear(settings[key]) + 'Z" transform="rotate(0)"></path></g>';
         }
     }
-    this.container.innerHTML += pathstr;
+    this.sung.innerHTML += pathstr;
 }
 
-function gear(d) {
+epiGear.prototype.gear = function (d) {
     var n = d.teeth,
         r2 = Math.abs(d.radius),
         r0 = r2 - 8,
@@ -98,5 +110,66 @@ function gear(d) {
     path.push('M0,', -r3, 'A', r3, ',', r3, ' 0,1,1,-0.1,', -r3);
     return path.join('');
 }
+
+epiGear.prototype.addEvent = function () {
+    var radios = document.getElementsByTagName('input');
+    var obj = this;
+    var sung = this.sung,
+        planetg = this.planetg,
+        annulusg = this.annulusg,
+        speed = this.speed,
+        settings = this.settings;
+
+    radios[0].onchange = function () {
+        var r0 = obj.now_r,
+            r1 = settings['annulus']['rotate_r'],
+            angle = (Date.now() - start) * speed;
+        planetg.setAttribute('transform', 'rotate(' + (offset += angle / r0 - angle / r1) + ')');
+        obj.now_r = r1;
+        timeEvent = setTimeout(obj.rotate(), 20);
+    }
+    radios[1].onchange = function () {
+        var r0 = obj.now_r,
+            r1 = settings['planet']['rotate_r'],
+            angle = (Date.now() - start) * speed;
+        planetg.setAttribute('transform', 'rotate(' + (offset += angle / r0 - angle / r1) + ')');
+        obj.now_r = r1;
+        timeEvent = setInterval(obj.rotate(), 20);
+    }
+    radios[2].onchange = function () {
+        var r0 = obj.now_r,
+            r1 = settings['sun']['rotate_r'],
+            angle = (Date.now() - start) * speed;
+        planetg.setAttribute('transform', 'rotate(' + (offset += angle / r0 - angle / r1) + ')');
+        obj.now_r = r1;
+        timeEvent = setInterval(obj.rotate(), 20);
+    }
+}
+
+epiGear.prototype.rotate = function () {
+    var sung = this.sung,
+        settings = this.settings,
+        speed = this.speed;
+    var obj = this;
+
+    var angle = (Date.now() - start) * speed;
+    var paths = sung.getElementsByTagName('g');
+    var thisobj;
+    for (var i = 0; i < paths.length; i++) {
+        thisobj = paths[i];
+
+        var key = thisobj.getAttribute('class'),
+            transform = 'rotate(' + (angle / (settings[key]['radius'])) + ')';
+        thisobj.getElementsByTagName('path')[0].setAttribute('transform', transform);
+        transform = 'rotate(' + (angle / obj.now_r) + ')';
+        sung.setAttribute('transform', transform);
+    }
+
+    // clearTimeout(timeEvent);
+    timeEvent = setTimeout(function () {
+        obj.rotate();
+    }, 20);
+}
+
 
 
