d11e751e7f341ab5a61189ad74f6322beafd7970
eWangsf <1342832773@qq.com>
2015-11-22 10:17:03 +0800

    epicyclic gearing

--- epiGear/d3/epiGear.js
+++ epiGear/d3/epiGear.js
@@ -13,9 +13,10 @@ var width = 960,
     y = Math.cos(2 * Math.PI / 3);
 
 var offset = 0,
-    speed = 4,
+    speed = 2,
     start = Date.now();
-  
+
+
 var svg = d3.select('body').append('svg')
     .attr({
         'width': width,
@@ -104,22 +105,23 @@ function gear(d) {
         r2 = Math.abs(d.radius),
         r0 = r2 - 8,
         r1 = r2 + 8,
-      
+        
         r3 = d.annulus ? (r3 = r0, r0 = r1, r1 = r3, r2 + 20) : 20,
         a0 = -Math.PI / 2 + (d.annulus ? Math.PI / n : 0),
       
         da = Math.PI / n,
         i = -1,
         path = ['M', r0 * Math.cos(a0), ',', r0 * Math.sin(a0)];
-    while (++i < n) 
-    path.push(
-        'A', r0, ',', r0, ' 0,0,1 ', r0 * Math.cos(a0 += da), ',', r0 * Math.sin(a0),
-        'L', r2 * Math.cos(a0), ',', r2 * Math.sin(a0),
-        'L', r1 * Math.cos(a0 += da / 3), ',', r1 * Math.sin(a0),
-        'A', r1, ',', r1, ' 0,0,1 ', r1 * Math.cos(a0 += da / 3), ',', r1 * Math.sin(a0),
-        'L', r2 * Math.cos(a0 += da / 3), ',', r2 * Math.sin(a0),
-        'L', r0 * Math.cos(a0), ',', r0 * Math.sin(a0)); 
-               //画内纹，中1线0线1/3内（外）1/3线1/3线0中1
+    while (++i < n) {
+        path.push(
+            'A', r0, ',', r0, ' 0,0,1 ', r0 * Math.cos(a0 += da), ',', r0 * Math.sin(a0),
+            'L', r2 * Math.cos(a0), ',', r2 * Math.sin(a0),
+            'L', r1 * Math.cos(a0 += da / 3), ',', r1 * Math.sin(a0),
+            'A', r1, ',', r1, ' 0,0,1 ', r1 * Math.cos(a0 += da / 3), ',', r1 * Math.sin(a0),
+            'L', r2 * Math.cos(a0 += da / 3), ',', r2 * Math.sin(a0),
+            'L', r0 * Math.cos(a0), ',', r0 * Math.sin(a0)); 
+    }
+    //画内纹，中1线0线1/3内（外）1/3线1/3线0中1
     path.push('M0,', -r3, 'A', r3, ',', r3, ' 0,1,1,-0.1,', -r3);//画外圆
     return path.join('');//得到一个path
 }
@@ -134,10 +136,6 @@ d3.selectAll('input[name=reference]')//选择三个radio元素
             frame.datum({radius: radius1});//每换一个模式变动整体转的半径率
         }
     );
-d3.selectAll('input[name=speed]')
-    .on('change', function() { 
-            speed = +this.value; 
-        });//选择速度
     
 d3.timer(function(){
     var angle = (Date.now() - start) * speed,
