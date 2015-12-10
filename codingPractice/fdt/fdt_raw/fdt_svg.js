b9a320965efb8e70a97f08c47b1b92a44242fef0
eWangsf <1342832773@qq.com>
2015-12-10 15:27:30 +0800

    dom

--- codingPractice/fdt/fdt_raw/fdt_svg.js
+++ codingPractice/fdt/fdt_raw/fdt_svg.js
@@ -1,12 +1,11 @@
 // svg
-var svg = document.getElementsByTagName('svg')[0];
-var width = svg.getAttribute('width'),
-    height = svg.getAttribute('height'),
+var svg;
+var width,
+    height,
     padding = 20;
 
 // circle styles
 var radium = 5,
-    linkcolor = '#999',
     circlecolor = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"],
     colors = [],
 // link styles
@@ -16,8 +15,8 @@ var radium = 5,
 
 var maxOffset = 50,
     offset = maxOffset + 1,
-    zero = 0.001,
-    charge = 3,
+    zero = .001,
+    charge = 10,
     linkDistance = 15,
     linkStrength = .0004;
 
@@ -26,68 +25,48 @@ var circles,
     links,
     circleNumber,
     linkNumber,
-    circledoms,
-    linkdoms;
+    circledoms = {},
+    linkdoms = {};
 
 
 window.onload = function () {
+    var svg = document.getElementsByTagName('svg')[0];
+    var width = svg.getAttribute('width'),
+        height = svg.getAttribute('height'),
+        padding = 20;
     circles = dataset.nodes;
     links = dataset.links;
     circleNumber = circles.length;
     linkNumber = links.length;
 
-    // for (var i = 0; i < circleNumber; i++) {
-    //     var g = parseInt(circles[i].group);
-    //     if(colors[g]) {continue;}
-    //     colors[g] = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
-    // }
-
-    // bind
-    // for (var i = 0; i < circleNumber; i++) {
-    //     circles[i].asSrc = [];
-    //     circles[i].asTar = [];
-    // }
-
-    // for (var i = 0; i < linkNumber; i++) {
-    //     var obj = links[i];
-    //     circles[obj.source].asSrc.push(i);
-    //     circles[obj.target].asTar.push(i);
-    // }
-
-
-    //init
-    var circlePos = [];
-    for (var i = 0; i < circleNumber; i++) { 
-        var x = (Math.random() - 0.5) * 10 * linkDistance + width / 2;
-        var y = (Math.random() - 0.5) * 10 * linkDistance  + height /2;
-        circlePos.push([x, y]);
-    }
-
-    var linkStr = '';
     for (var i = 0; i < linkNumber; i++) {
-        var source = links[i].source;
-        var target = links[i].target;
-        linkStr += '<line x1="' + circlePos[source][0] + '" y1="' + circlePos[source][1] + '" x2="' 
-                + circlePos[target][0] + '" y2="' + circlePos[target][1] 
-                + '" style="stroke:' + linkcolor + ';stroke-width: ' 
-                + Math.sqrt(links[i].value) + '"/>'
-    }
-    svg.innerHTML += linkStr;
-    linkdoms = document.getElementsByTagName('line');
+        var linkele = document.createElementNS('http://www.w3.org/2000/svg','line');
+        circles[links[i].source].x = circles[links[i].source].x || ((Math.random() - 0.5) * 5 * linkDistance + width / 2);
+        circles[links[i].source].y = circles[links[i].source].y || ((Math.random() - 0.5) * 5 * linkDistance  + height / 2);
 
-    var circleStr = '';
+        circles[links[i].target].x = circles[links[i].target].x || ((Math.random() - 0.5) * 5 * linkDistance + width / 2);
+        circles[links[i].target].y = circles[links[i].target].y || ((Math.random() - 0.5) * 5 * linkDistance  + height / 2);
+        linkele.setAttribute('x1', circles[links[i].source].x);
+        linkele.setAttribute('y1', circles[links[i].source].y);
+        linkele.setAttribute('x2', circles[links[i].target].x);
+        linkele.setAttribute('y2', circles[links[i].target].y);
+        linkele.setAttribute('style', 'stroke-width: ' + Math.sqrt(links[i].value));
+        svg.appendChild(linkele);
+        linkdoms[i] = linkele;
+    }
+
     for (var i = 0; i < circleNumber; i++) {
-        circleStr += '<circle cx="' + circlePos[i][0] + '" cy="' + circlePos[i][1] + '" r="' + radium + '" fill="' + circlecolor[circles[i].group] + '" />'
-        // circleStr += '<circle cx="' + circlePos[i][0] + '" cy="' + circlePos[i][1] + '" r="' + radium + '" fill="' + colors[circles[i].group] + '" />'
+        var circleele = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        circleele.setAttribute('cx', circles[i].x);
+        circleele.setAttribute('cy', circles[i].y);
+        circleele.setAttribute('r', radium);
+        circleele.setAttribute('fill', circlecolor[circles[i].group]);
+        svg.appendChild(circleele);
+        circledoms[i] = circleele;
     }
-    svg.innerHTML += circleStr;
-    circledoms = document.getElementsByTagName('circle');
-    
-    // while (offset >= maxOffset) {
-    //     offset = 0;
-        toBeBalance();
-    // }
 
+    
+    var clock = setTimeout(toBeBalance, 0);
 }
 
 function toBeBalance() {
@@ -98,12 +77,6 @@ function toBeBalance() {
         while (++j < circleNumber) {
             repulse(i, j);
         }
-    }
-
-    // attract
-    var j = -1;
-    while (++j < linkNumber) {
-        attract(j);
     }
 
     // line 
@@ -120,9 +93,13 @@ function toBeBalance() {
         thislink.setAttribute('y2', y2);
     }
 
-    setTimeout(toBeBalance, 8);
+    // attract
+    var j = -1;
+    while (++j < linkNumber) {
+        attract(j);
+    }
 
-    
+    clock = setTimeout(toBeBalance, 0);
 }
 
 function repulse(node1, node2) {
@@ -135,23 +112,17 @@ function repulse(node1, node2) {
     var repweight = charge / dn;
     var x = repweight * dx;
     var y = repweight * dy;
-    // offset += Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-    // console.log(offset);
-    // console.log(x);
     if((Math.abs(x) < zero) || (Math.abs(y) < zero)) {
         return;
     }
 
     circledoms[node1].setAttribute('cx', parseFloat(circledoms[node1].getAttribute('cx')) - x);
     circledoms[node1].setAttribute('cy', parseFloat(circledoms[node1].getAttribute('cy')) - y);
-    
     circledoms[node2].setAttribute('cx', parseFloat(circledoms[node2].getAttribute('cx')) + x);
     circledoms[node2].setAttribute('cy', parseFloat(circledoms[node2].getAttribute('cy')) + y);
-
 }
 
 function attract(link) {
-    // console.log(links[link]);
     var srcircle = circledoms[links[link].source],
         tarcircle = circledoms[links[link].target],
         sr_x = linkdoms[link].getAttribute('x1') || 0,
@@ -161,11 +132,12 @@ function attract(link) {
         dx = (tar_x - sr_x) - (linkDistance), dy = (tar_y - sr_y) - (linkDistance), dn = dx * dx + dy * dy,
         attractweight = linkStrength * Math.sqrt(dn);
 
+    if((Math.abs(attractweight) < zero)) {
+        return;
+    }
+
     srcircle.setAttribute('cx', parseFloat(srcircle.getAttribute('cx')) + attractweight * dx);
     srcircle.setAttribute('cy', parseFloat(srcircle.getAttribute('cy')) + attractweight * dy);
     tarcircle.setAttribute('cx', parseFloat(tarcircle.getAttribute('cx')) - attractweight * dx);
     tarcircle.setAttribute('cy', parseFloat(tarcircle.getAttribute('cy')) - attractweight * dy);
-    // console.log('dx= ' + dx + '   dy= ' + dy + '   attr_x= ' + attractweight * dx + '   attr_y= ' + attractweight * dy);
-        
-    // console.log(repweight * Math.sqrt(dx));
 }
